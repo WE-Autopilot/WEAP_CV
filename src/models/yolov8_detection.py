7de6903f662412e87bc1de4bf41519340f464d3c
Obaid Mohiuddin <145414726+obaidmm@users.noreply.github.com>
2025-02-04 13:54:58 -0500

    Update yolov8_detection.py

--- src/models/yolov8_detection.py
+++ src/models/yolov8_detection.py
@@ -1,15 +1,3 @@
 """
-baseline_model.py - Load and test a pre-trained YOLOv8 model
-
-Running this code will load a YOLOv8 model and test it on a sample image named test.png.
+There is an image in datasets to test your YOLOv8 Model. 
 """
-
-from ultralytics import YOLO
-
-# Load a pre-trained YOLOv8 model
-model = YOLO("yolov8n.pt")  # Smallest YOLOv8 model
-
-# Test inference on a sample image
-results = model("datasets/test.png")
-
-
